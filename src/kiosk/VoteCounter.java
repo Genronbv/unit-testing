0156b4f7dba89dae6a706d62fe3412bd7cd3a04e
Genronbv <genisborras@gmail.com>
2019-01-13 20:22:50 +0100

    Add methodes to VoteCounter

--- src/kiosk/VoteCounter.java
+++ src/kiosk/VoteCounter.java
@@ -1,4 +1,32 @@
 package kiosk;
 
+import data.Party;
+
+import java.util.*;
+
+/**
+ * A class that represents the result in an election site
+ */
 public class VoteCounter {
+
+    private int countNull = 0;
+    private int countBlank = 0;
+    private int countVotes = 0;
+
+    public VoteCounter(Set<Party> Parties) {
+
+    }
+    public void countParty(Party party){
+
+    }
+    public void countNull() { }
+    public void countVotes() {}
+    public void countBlank() {}
+    public void scrutinize(Party party){
+
+    }
+    public int getVotesFor(Party party) {return 0;}
+    public int getNulls() { return this.countNull; }
+    public int getBlanks() { return this.countBlank; }
+    public int getTotal() { return this.countNull + this.countVotes + this.countBlank; }
 }
