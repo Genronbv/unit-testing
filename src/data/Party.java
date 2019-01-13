485ec32b34240c957a7c0228bcc85f4db9dd9a1c
Genronbv <genisborras@gmail.com>
2019-01-13 19:59:24 +0100

    Add class Party and exceptions

---
+++ src/data/Party.java
@@ -0,0 +1,28 @@
+package data;
+
+import exceptions.NoNameException;
+
+/**
+ * The name of a party that participates in an election.
+ */
+final public class Party {
+    private final String name;
+    public Party(String name) throws NoNameException {
+        if(name == null) throw new NoNameException();
+        this.name = name;
+    }
+    public String getName() { return name; }
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Party party1 = (Party) o;
+        return name.equals(party1.name);
+    }
+    @Override
+    public int hashCode() { return name.hashCode(); }
+    @Override
+    public String toString() {
+        return "Party{" + "name='" + name + '\'' + '}';
+    }
+}
---
+++ src/exceptions/NoNameException.java
@@ -0,0 +1,4 @@
+package exceptions;
+
+public class NoNameException extends Exception {
+}
