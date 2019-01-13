96c0968cb1da09dc63ca46d6f08fe37fd0f47e7c
Genronbv <genisborras@gmail.com>
2019-01-13 20:05:48 +0100

    Add class MailAddress and exceptions

---
+++ src/data/MailAddress.java
@@ -0,0 +1,28 @@
+package data;
+
+import exceptions.NoMailException;
+
+final public class MailAddress {
+    private final String mail;
+    public MailAddress(String mail) throws NoMailException {
+        if(mail == null || mail.equals("")) throw new NoMailException();
+        this.mail = mail;
+    }
+    public String getMail(){ return this.mail;}
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        MailAddress mail1 = (MailAddress) o;
+        return mail.equals(mail1.mail);
+    }
+    @Override
+    public int hashCode() { return mail.hashCode(); }
+    @Override
+    public String toString() {
+        return "Mail{" + "mail='" + mail + '\'' + '}';
+    }
+
+
+}
