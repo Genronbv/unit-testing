6fedce062f9bc57110975a747a93ae619f4ae672
Genronbv <genisborras@gmail.com>
2019-01-13 20:02:26 +0100

    Add class Nif and exceptions

---
+++ src/data/Nif.java
@@ -0,0 +1,30 @@
+package data;
+
+import exceptions.NoNifException;
+
+final public class Nif {
+
+    private final String nif;
+    public Nif(String nif) throws NoNifException {
+
+        if(nif == null || nif.equals("")) throw new NoNifException();
+        this.nif = nif;
+    }
+    public String getNif(){ return this.nif;}
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Nif nif1 = (Nif) o;
+        return nif.equals(nif1.nif);
+    }
+    @Override
+    public int hashCode() { return nif.hashCode(); }
+    @Override
+    public String toString() {
+        return "Nif{" + "nif='" + nif + '\'' + '}';
+    }
+
+
+}
---
+++ src/exceptions/NoNifException.java
@@ -0,0 +1,4 @@
+package exceptions;
+
+public class NoNifException extends Exception {
+}
