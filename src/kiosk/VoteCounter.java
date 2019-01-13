0d52e103d1864a2e25e9e947e06cc4184f7e3784
Genronbv <genisborras@gmail.com>
2019-01-13 20:25:41 +0100

    Fix warnings in VoteCounter

--- src/kiosk/VoteCounter.java
+++ src/kiosk/VoteCounter.java
@@ -1,6 +1,7 @@
 package kiosk;
 
 import data.Party;
+import exceptions.NoPartyException;
 
 import java.util.*;
 
@@ -9,23 +10,37 @@ import java.util.*;
  */
 public class VoteCounter {
 
+    private Map<Party, Integer> validParties = new HashMap<>();
     private int countNull = 0;
     private int countBlank = 0;
     private int countVotes = 0;
 
+
     public VoteCounter(Set<Party> Parties) {
+        for (Party party: Parties){
+            validParties.put(party,0);
+        }
+
 
     }
-    public void countParty(Party party){
-
+    private void countParty(Party party) throws NoPartyException {
+        if (this.validParties.containsKey(party)){
+            int votesForParty = this.validParties.get(party);
+            this.validParties.replace(party,votesForParty+1);
+            countVotes();
+        }
+        else{
+            throw new NoPartyException();}
     }
-    public void countNull() { }
-    public void countVotes() {}
-    public void countBlank() {}
-    public void scrutinize(Party party){
-
+    private void countNull() { this.countNull++;}
+    private void countVotes() {this.countVotes++;}
+    private void countBlank() { this.countBlank++; }
+    private void scrutinize(Party party) throws NoPartyException {
+        if (party.getName().equals("null")){ countNull();}
+        else if (party.getName().equals("")){ countBlank();}
+        else {countParty(party);}
     }
-    public int getVotesFor(Party party) {return 0;}
+    public int getVotesFor(Party party) { return this.validParties.get(party); }
     public int getNulls() { return this.countNull; }
     public int getBlanks() { return this.countBlank; }
     public int getTotal() { return this.countNull + this.countVotes + this.countBlank; }
